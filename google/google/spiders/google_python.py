f42a42300844b76bbb4927ea7d0443b2f3287cb4
DDDlucky <d.ono2105@gmail.com>
2021-11-12 13:29:42 +0900

    11.12

---
+++ google/google/spiders/google_python.py
@@ -0,0 +1,16 @@
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+class GooglePythonSpider(scrapy.Spider):
+    name = 'google_python'
+
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='http://www.google.com/?hl=ja',
+            wait_time=3,
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        driver = response.meta['driver']
+        driver.save_screenshot('01_open_google.png')
---
+++ selenium/selenium_test.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
+driver.get('https://www.bing.com/')
+
+
+
